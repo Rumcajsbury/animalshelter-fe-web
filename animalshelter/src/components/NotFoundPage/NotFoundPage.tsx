8bce1ebca21f0582672a16cc0b91a6185fd40243
Krzysztof Radwanski <Krzysztof_Radwanski@epam.com>
2019-12-11 23:00:31 +0100

    working on NotFoundPage

--- animalshelter/src/components/NotFoundPage/NotFoundPage.tsx
+++ animalshelter/src/components/NotFoundPage/NotFoundPage.tsx
@@ -1,8 +1,11 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 const NotFoundPage: React.FC = () => {
   return (
-      <h1>not found page</h1>
+    <div className="mt-3">
+      <h1>Nie znaleziono strony</h1>
+      <Link to="/" className="btn btn-primary" >Wróć do strony głównej</Link>
+    </div>
   );
 };
 
